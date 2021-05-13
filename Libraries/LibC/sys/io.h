b23faf5be66e66512db5b6b888c3ff3e4078d9f2
barnden <brandon@barnden.dev>
2021-05-13 13:47:40 -0500

    LibC: Add io.h
    More specifically, implement outb and inb.

--- Libraries/LibC/sys/io.h
+++ Libraries/LibC/sys/io.h
@@ -2,6 +2,18 @@
 
 #include <FSH/Types.h>
 
-static inline void outb(u16 port, u8 val) {
-    asm ("outb %0, %1" : : "a"(val), "Nd"(port));
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+static inline void outb(u16 port, u8 val) { asm("outb %0, %1" : : "a"(val), "Nd"(port)); }
+
+static inline u8 inb(u16 port) {
+    auto ret = u8 {};
+
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+
+    return ret;
 }
+
+__END_DECLS
