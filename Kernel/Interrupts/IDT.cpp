14d4ec91c8cd9be5eb843efe268914c092e91b16
barnden <brandon@barnden.dev>
2021-05-13 13:52:33 -0500

    Kernel: Load IDT using asm function over inline asm

--- Kernel/Interrupts/IDT.cpp
+++ Kernel/Interrupts/IDT.cpp
@@ -1,38 +1,28 @@
 #include <FSH/Platform.h>
 #include <FSH/Types.h>
 
-#include <Kernel/Interrupts/IDT.h>
+#include <Kernel/Interrupts/Interrupts.h>
 
 #include <string.h>
 
-struct PACKED IDT_entry {
-    u16 base_lo;
-    u16 sel;
-    u8 zero;
-    u8 flags;
-    u16 base_hi;
-};
-
-struct PACKED IDT_ptr {
-    u16 limit;
-    u32 base;
-};
+extern "C" void load_idt(u32);
 
 IDT_entry idt[256];
 
 void idt_set_gate(u8 num, u32 base, u16 sel, u8 flags) {
-    u16 base_lo = base & 0xFFFF;
-    u16 base_hi = (base >> 0xF) & 0xFFFF;
-
-    idt[num] = IDT_entry {
-        .base_lo = base_lo, .sel = sel, .zero = 0, .flags = flags, .base_hi = base_hi
-    };
+    idt[num].base_lo = base & 0xFFFF;
+    idt[num].base_hi = (base >> 0x10) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].zero = 0;
+    idt[num].flags = flags;
 }
+
+IDT_entry const& idt_get_gate(size_t i) { return idt[i]; }
 
 void idt_install() {
     IDT_ptr idtp = { .limit = (sizeof(IDT_entry) * 256) - 1, .base = reinterpret_cast<u32>(&idt) };
 
-    memset(reinterpret_cast<void*>(&idt), 0, idtp.limit + 1);
+    memset(static_cast<void*>(&idt), 0, idtp.limit + 1);
 
-    __asm__("lidt %0" : : "m"(idtp));
+    load_idt(reinterpret_cast<u32>(&idtp));
 }
