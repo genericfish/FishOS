84bcd372eaacdb08266e82932e3f33838f9b0c20
barnden <brandon@barnden.dev>
2021-06-04 15:15:49 -0500

    LibC: Improve consistency by using __restrict

--- Libraries/LibC/string.h
+++ Libraries/LibC/string.h
@@ -6,7 +6,7 @@
 __BEGIN_DECLS
 
 int memcmp(void const* s1, void const* s2, size_t n);
-void* memcpy(void* __restrict__ dest, void const* __restrict src, size_t n);
+void* memcpy(void* __restrict dest, void const* __restrict src, size_t n);
 void* memmove(void* dest, void const* src, size_t n);
 void* memset(void* s, int c, size_t n);
 size_t strlen(char const* s);
