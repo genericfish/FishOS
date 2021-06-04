0af1d3b4880526468c1e18d49e5295f8fd3020d4
barnden <brandon@barnden.dev>
2021-06-04 15:15:20 -0500

    Kernel: Update main function

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -1,24 +1,25 @@
 #include <FSH/Platform.h>
 #include <FSH/Types.h>
+#include <FSH/Utility.h>
 
-#include <Kernel/Interrupts/Interrupts.h>
+// #include <Kernel/Interrupts/Interrupts.h>
 #include <Kernel/TTY/TTY.h>
 
 #include <stdio.h>
 #include <sys/io.h>
 
-extern "C" {
-
-void main()
+extern "C" int main()
 {
-    terminal_initialize();
+    TTY::clear(VGA::vga_entry_color(VGA::WHITE, VGA::BLUE));
 
-    gdt_install();
-    idt_install();
-    isr_install();
-    irq_install();
+    // TODO/FIXME: Implement interrupts!
+    // idt_install();
+    // isr_install();
+    // irq_install();
 
-    asm("sti");
+    // irq_set(0, timer);
+
+    // asm("sti");
 
     printf("      ===================          ====\n");
     printf("    ===                 ===       == ==\n");
@@ -29,7 +30,19 @@ void main()
     printf("      ===================          ====\n");
     printf("            f i s h\n");
 
+    // TEST: printf formatting
+    printf("Expect -16: %d\nExpect 29: %d\n", -16, 29);
+    printf("Expect DEADBEEF: %x\n", 0xdeadbeef);
+    printf("Expect 777: %o\n", 0777);
+    printf("Expect 11: %b\n", 3);
+
+    // TEST: Compile time loops using integer packs (remove this later).
+    [&]<size_t... I>(index_sequence<I...>) {
+        (printf("%d\n", I), ...);
+    }(make_index_sequence<5>{});
+
     while (true)
         ;
-}
+
+    return 0;
 }
