2bc3ff2c14bf4d29c9ee1373a2b14f4423834c0b
barnden <brandon@barnden.dev>
2021-08-09 10:08:34 -0500

    Kernel: Verify multiboot compliant bootloader
    - The bootloader magic and multiboot_info_t struct address was popped into edi/esi.
    We read the register values as multiboot_magic and multiboot_header_ptr in main.
    - Verify multiboot_magic is MULTIBOOT_BOOTLOADER_MAGIC
    - Verify correct flag is set in multiboot_info_t from multiboot_header_ptr

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -5,8 +5,9 @@
 #include <Kernel/Interrupts/Interrupts.h>
 #include <Kernel/TTY/TTY.h>
 
-#include <Kernel/Memory/Paging.h>
 #include <Kernel/Memory/Address.h>
+
+#include <Kernel/multiboot.h>
 
 #include <stdio.h>
 #include <sys/io.h>
@@ -16,19 +17,23 @@ using namespace Memory;
 
 u64 ticks = 0;
 
-void timer(Interrupts::Registers)
-{
-    if (ticks++ % 18 == 0)
-        printf("%lld seconds.\n", ticks / 18);
-}
-
-extern "C" int main()
+extern "C" int main(u64 multiboot_magic, u64 multiboot_header_ptr)
 {
     TTY::clear(VGA::vga_entry_color(VGA::WHITE, VGA::BLUE));
 
-    Interrupts::setup();
+    if (multiboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("Non-multiboot compliant bootloader. (%llx, %llx)\n", multiboot_magic, multiboot_header_ptr);
+        return -1;
+    }
 
-    irq_set(0, timer);
+    auto const& header = *reinterpret_cast<multiboot_info_t*>(multiboot_header_ptr + g_virtual_base_address);
+
+    if (!(header.flags & (1 << 6))) {
+        printf("No multiboot mmap provided.\n");
+        return -1;
+    }
+
+    Interrupts::setup();
 
     asm("sti");
 
