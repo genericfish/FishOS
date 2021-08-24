be482b35a255f0af6c076304180c66d00ba87aba
barnden <brandon@barnden.dev>
2021-08-24 18:12:07 -0500

    Kernel: Get multiboot tag type based on flag.

--- Kernel/Multiboot.h
+++ Kernel/Multiboot.h
@@ -25,18 +25,102 @@ consteval size_t get_multiboot_flag_from_type()
     return MULTIBOOT_TAG_TYPE_END;
 }
 
-template <typename T>
-FLATTEN T* get_multiboot_tag(u64 addr)
-{
-    auto* cur = reinterpret_cast<multiboot_tag*>(addr + 8);
-    auto const flag = get_multiboot_flag_from_type<T>();
+template <size_t N>
+struct multiboot_type {
+    using type = multiboot_tag;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_CMDLINE> {
+    using type = multiboot_tag_string;
+};
 
-    while (true) {
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME> {
+    using type = multiboot_tag_string;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_MODULE> {
+    using type = multiboot_tag_module;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_BASIC_MEMINFO> {
+    using type = multiboot_tag_basic_meminfo;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_BOOTDEV> {
+    using type = multiboot_tag_bootdev;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_MMAP> {
+    using type = multiboot_tag_mmap;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_VBE> {
+    using type = multiboot_tag_vbe;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_FRAMEBUFFER> {
+    using type = multiboot_tag_framebuffer;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_ELF_SECTIONS> {
+    using type = multiboot_tag_elf_sections;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_APM> {
+    using type = multiboot_tag_apm;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_EFI32> {
+    using type = multiboot_tag_efi32;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_EFI64> {
+    using type = multiboot_tag_efi64;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_SMBIOS> {
+    using type = multiboot_tag_smbios;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_ACPI_OLD> {
+    using type = multiboot_tag_old_acpi;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_ACPI_NEW> {
+    using type = multiboot_tag_new_acpi;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_NETWORK> {
+    using type = multiboot_tag_network;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_EFI_MMAP> {
+    using type = multiboot_tag_efi_mmap;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_EFI32_IH> {
+    using type = multiboot_tag_efi32_ih;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_EFI64_IH> {
+    using type = multiboot_tag_efi64_ih;
+};
+template <>
+struct multiboot_type<MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR> {
+    using type = multiboot_tag_load_base_addr;
+};
+
+template<size_t N>
+FLATTEN multiboot_type<N>::type* get_multiboot_tag(u64 addr) {
+    auto* cur = reinterpret_cast<multiboot_tag*>(addr + 8);
+
+    while(true) {
         if (cur->type == MULTIBOOT_TAG_TYPE_END)
             return nullptr;
 
-        if (cur->type == flag)
-            return reinterpret_cast<T*>(cur);
+        if (cur->type == N)
+            return reinterpret_cast<multiboot_type<N>::type*>(cur);
 
         cur = reinterpret_cast<multiboot_tag*>(reinterpret_cast<u8*>(cur) + ((cur->size + 7) & ~7));
     }
