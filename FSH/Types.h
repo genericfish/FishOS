d4f9128ebd281a4a9be338dedbbcaf66306f83a9
barnden <brandon@barnden.dev>
2021-08-23 19:21:43 -0500

    FSH/Types: Add integral_constant, bool_constant, false_type, true_type

--- FSH/Types.h
+++ FSH/Types.h
@@ -2,13 +2,13 @@
 
 // FIXME: Remove when Intellisense supports GNU++23 (g++11).
 #ifndef __INTELLISENSE__
-#ifdef __linux__
-#error "[ERROR] Kernel: Compile using the cross-compiler."
-#endif
+#    ifdef __linux__
+#        error "[ERROR] Kernel: Compile using the cross-compiler."
+#    endif
 
-#ifndef __x86_64__
-#error "[ERROR] Kernel: Compile using x86_64-elf cross-compiler."
-#endif
+#    ifndef __x86_64__
+#        error "[ERROR] Kernel: Compile using x86_64-elf cross-compiler."
+#    endif
 #else
 /*
 This is invalid C++, user-defined literals must be prefixed with an underscore.
@@ -19,7 +19,7 @@ constexpr char operator"" UZ(unsigned long long);
 #endif
 
 #ifdef restrict
-#undef restrict
+#    undef restrict
 #endif
 #define restrict __restrict
 
@@ -52,13 +52,13 @@ using int64_t = i64;
 using ptrdiff_t = __PTRDIFF_TYPE__;
 #else
 
-#include <stddef.h>
-#include <stdint.h>
-#include <sys/types.h>
+#    include <stddef.h>
+#    include <stdint.h>
+#    include <sys/types.h>
 
-#ifdef __ptrdiff_t
+#    ifdef __ptrdiff_t
 using ptrdiff_t = __PTRDIFF_TYPE__;
-#endif
+#    endif
 
 #endif
 
@@ -66,3 +66,20 @@ constexpr u64 KiB = 1ULL << 10;
 constexpr u64 MiB = 1ULL << 20;
 constexpr u64 GiB = 1ULL << 30;
 constexpr u64 TiB = 1ULL << 40;
+
+template <class T, T v>
+struct integral_constant {
+    T static constexpr value = v;
+
+    using value_t = T;
+    using self_t = integral_constant;
+
+    constexpr operator value_t() const noexcept { return value; }
+    constexpr value_t operator()() const noexcept { return value; }
+};
+
+template <bool B>
+using bool_constant = integral_constant<bool, B>;
+
+using false_type = bool_constant<false>;
+using true_type = bool_constant<true>;
